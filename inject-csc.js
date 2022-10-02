16ba269fb75b96b92081f43d6a3ae43c30dc4878
henry <henry@henrys-laptop.local>
2022-10-02 08:12:51 +0100

    ci fs

--- inject-csc.js
+++ inject-csc.js
@@ -2,7 +2,23 @@ var fs = require('fs');
 
 console.log('running inject-csc');
 
-var writeStream = fs.createWriteStream('./out/injection.html');
-writeStream.write('<div>Injected</div>');
-// writeStream.write("Thank You.");
-writeStream.end();
+/*
+pre: timer dist files to static. 
+read timer code file name.
+read .out/index.html √
+After target div add script to latest timer code. *
+write to .out/index.html
+*/
+// function readData(err, data) {
+//   return data;
+// }
+const indexPage = '.out/index.html';
+//const indexPage = 'next-env.d.ts' //testing
+fs.readFile(indexPage, 'utf8', (err, data) => {
+  var writeStream = fs.createWriteStream('./out/index.html');
+
+  writeStream.write(data);
+  writeStream.write('<div>Injected</div>');
+  // writeStream.write("Thank You.");
+  writeStream.end();
+});
