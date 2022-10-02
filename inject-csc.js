ebed40a3573e214e74db74e94e3b87f4d0e04ba7
henry <henry@henrys-laptop.local>
2022-10-02 08:24:38 +0100

    ci fs sync

--- inject-csc.js
+++ inject-csc.js
@@ -7,18 +7,23 @@ pre: timer dist files to static.
 read timer code file name.
 read .out/index.html √
 After target div add script to latest timer code. *
-write to .out/index.html
+write to .out/index.html √
 */
 // function readData(err, data) {
 //   return data;
 // }
-const indexPage = '.out/index.html';
+const indexPage = './out/index.html';
 //const indexPage = 'next-env.d.ts' //testing
-fs.readFile(indexPage, 'utf8', (err, data) => {
-  var writeStream = fs.createWriteStream('./out/index.html');
 
-  writeStream.write(data);
-  writeStream.write('<div>Injected</div>');
-  // writeStream.write("Thank You.");
-  writeStream.end();
-});
+var data = fs.readFileSync(indexPage);
+data += '<div>Injected+</div>';
+fs.writeFileSync(indexPage, data);
+
+// fs.readFile(indexPage, 'utf8', (err, data) => {
+//   var writeStream = fs.createWriteStream(indexPage);
+
+//   writeStream.write(data);
+//   writeStream.write('<div>Injected</div>');
+//   // writeStream.write("Thank You.");
+//   writeStream.end();
+// });
